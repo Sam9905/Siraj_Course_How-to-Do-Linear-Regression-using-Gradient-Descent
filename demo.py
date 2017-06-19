7dd3c377fdc376a359423eb10f2650059ef94d03
Cesar M <cesardedios94@gmail.com>
2017-06-19 08:22:07 -0400

    finalized equation for partial derivitives for m and b

--- demo.py
+++ demo.py
@@ -15,6 +15,15 @@ def step_gradient(b_current, m_current, points, learning_rate)
     #gradient descent
 	b_gradient = 0
 	m_gradient = 0
+	M = float(len(points))
+	for i in range(0, len(points)):
+		x = points[i,0]
+		y = points[i,1]
+		b_gradient += -(2/N) * (y - ((m_current * x) + b_current))
+		m_gradient += -(2/N) * x * (y - ((m_current * x) + b_current))
+	new_b = b_current - (learning_rate * b_gradient)
+	new_m = m_current - (learning_rate * m_gradient)
+	return [new_b, new_m]
 	
 
 def gradient_descent_runner(points, learning_rate, initial_b, initial_m, num_iterations)
